ae5e07362365222a8c27208cb43d684eede693d9
Angie-Ibarra <angelinasibarra@utexas.edu>
2020-02-12 23:04:42 -0600

    Wrote timedBlink function

---
+++ Lab1_Blink/Lab1_Blink/Lab1_Blink.ino
@@ -0,0 +1,33 @@
+#define LED_PIN 2
+
+void setup() {
+  // initialize digital pin LED_PIN as an output.
+  pinMode(LED_PIN, OUTPUT);
+
+}
+void timedBlink(int interval) {
+  digitalWrite(LED_PIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(interval);                       // wait for a second
+  digitalWrite(LED_PIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(interval);                       // wait for a second
+}
+
+void blinky() {
+  timedBlink(250);
+  timedBlink(500);
+  timedBlink(1000);
+}
+void dimmer(int freq, int duty) {
+  int period, onTime, offTime;
+  period = 1000/freq;
+  onTime = period * duty / 100;
+  offTime = period - onTime;
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(onTime);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(offTime);
+}
+void loop() {
+  blinky();
+
+}
